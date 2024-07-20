47cfe11af85f21b001153cb2ba2652a1c19ed30c
Nishith Jain <KingNish@users.noreply.huggingface.co>
2024-07-20 10:14:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -229,7 +229,7 @@ demo = gr.ChatInterface(fn=respond, title="OpenGPT 4o mini", textbox=gr.Multimod
                         examples=[["Hy, who are you?"],
                                   ["What's the current price of Bitcoin"], 
                                   ["Write me a Python function to calculate the first 10 digits of the fibonacci sequence."],
-                                  ["Create A Beautiful image of Effiel Tower at Night"]
+                                  ["Create A Beautiful image of Effiel Tower at Night"],
                                   ["What's the colour of Car in given image"]])
 
 demo.launch()
